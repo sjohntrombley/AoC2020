f800999599e754341ddde999278ebe8921e1a911
S. John Trombley <s.john.trombley@gmail.com>
2020-12-13 13:36:37 -0700

    Solved day 11

--- 11/Main.hs
+++ 11/Main.hs
@@ -1,37 +1,32 @@
 module Main where
 
 import Control.Monad (ap)
-import Data.Array (listArray, bounds, (!), (//), elems, {-debugging-} Array)
+import Data.Array (listArray, bounds, (!), (//), elems)
+import Data.Bool (bool)
 import Data.List ((\\), foldl')
+import Data.Maybe (catMaybes)
 import System.IO (IOMode(ReadMode), openFile, hGetContents)
 
-type Room = Array (Int, Int) Char
-
-update :: Room -> Room -> (Int, Int) -> Room
 update old new (r, c)
     | old!(r, c) == 'L' = updateEmpty r c old new
     | old!(r, c) == '#' = updateFull r c old new
     | old!(r, c) == '.' = new
     | otherwise = error "Invalid character."
 
-updateEmpty :: Int -> Int -> Room -> Room -> Room 
 updateEmpty r c old new
     = let surs = getSurroundings (fst.snd $ bounds old) (snd.snd $ bounds old) r c
       in if length (filter (isFull old) surs) == 0
          then new // [((r, c),'#')]
          else new
 
-updateFull :: Int -> Int -> Room -> Room -> Room 
 updateFull r c old new
     = let surs = getSurroundings (fst.snd $ bounds old) (snd.snd $ bounds old) r c
       in if length (filter (isFull old) surs) > 3
          then new // [((r, c),'L')]
          else new
 
-isFull :: Room -> (Int, Int) -> Bool
 isFull old loc = old!loc == '#'
 
-getSurroundings :: Int -> Int -> Int -> Int -> [(Int, Int)]
 getSurroundings r_max c_max r c 
     | (r, c) == (0, 0) = [(r', c') | r' <- [0..1], c' <- [0..1]] \\ [(r, c)]
     | (r, c) == (0, c_max) = [(r', c') | r' <- [0..1], c' <- [c-1..c]] \\ [(r, c)]
@@ -44,21 +39,64 @@ getSurroundings r_max c_max r c
     | otherwise = [(r', c') | r' <- [r-1..r+1], c'<- [c-1..c+1]] \\ [(r, c)]
 
 
-iterRoom :: Room -> Room
 iterRoom old 
     = let r_max = fst.snd $ bounds old
           c_max = snd.snd $ bounds old
       in foldl' (update old) old [(r, c) | r <- [0..r_max], c <- [0..c_max]]
 
-part1 :: Room -> IO ()
 part1 room
     = let allIters = iterate iterRoom room
           findStop = zipWith (ap ((.).(,)) (/=)) allIters $ tail allIters
       in putStrLn.show.length.filter ('#'==).elems.fst.head $ dropWhile snd findStop
 
+--part2 :: Room -> IO ()
+part2 room
+    = let allIters = iterate nextRoom room
+          findStop = zipWith (ap ((.).(,)) (/=)) allIters $ tail allIters
+      in putStrLn.show.length.filter ('#'==).elems.fst.head $ dropWhile snd findStop
+
+--nextRoom :: Room -> Room
+nextRoom room
+    = let (rmax, cmax) = snd $ bounds room
+          indices = [(r, c) | r <- [0..rmax], c <- [0..cmax]]
+      in room // catMaybes (map (update2 room) indices)
+
+--update2 :: Room -> (Int, Int) -> Maybe ((Int, Int), Char)
+update2 room loc
+    | room!loc=='#' && numFilled room loc>4 = Just (loc, 'L')
+    | room!loc=='L' && numFilled room loc==0 = Just (loc, '#')
+    | otherwise = Nothing
+
+--numFilled :: Room -> (Int, Int) -> Int
+numFilled
+    = let dirs = [(dr, dc) | dr <- [-1..1], dc <- [-1..1]] \\ [(0, 0)]
+      in ((.) $ length.flip filter dirs).isFilledDir
+
+--isFilledDir :: Room -> (Int, Int) -> (Int, Int) -> Bool
+isFilledDir room (r, c) (dr, dc)
+    = let offsets = [(r+dr*o, c+dc*o) | o <- [1..getMaxOffset room dr dc r c]]
+      in foldr (ap (flip.bool.('#'==)) ('.'==).(room!)) False offsets
+-- (ap (flip.bool.('#'==)) ('.'==).(room!)) loc b
+--     = if room!loc == '.' then b else room!loc == '#'
+
+-- getMaxOffset :: Room -> Int -> Int -> Int -> Int -> Int
+getMaxOffset room dr dc r c
+    | dr<0 && dc<0 = min r c
+    | dr<0 && dc>0 = min r (cmax - c)
+    | dr<0         = r
+    | dr>0 && dc<0 = min (rmax - r) c
+    | dr>0 && dc>0 = min (rmax - r) (cmax-c)
+    | dr>0         = rmax - r
+    | dc<0         = c
+    | dc>0         = cmax - c
+    | otherwise    = error "You aren't looking in a direction"
+    where rmax = fst.snd $ bounds room
+          cmax = snd.snd $ bounds room
+
 main = do f <- openFile "input.txt" ReadMode
           c <- hGetContents f
           let room = listArray ((0,0), ((-1+).length $ lines c, (-1+).length.head $ lines c)).concat $ lines c
-          putStrLn $ show room
           putStr "Part 1:\n\t"
           part1 room
+          putStr "Part 2:\n\t"
+          part2 room
---
+++ 11/README.md
@@ -0,0 +1,5 @@
+# Day 10
+## Part 1
+Part 1 was relatively simple, just [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) with tweaked rules and holes in the board. I'll get around to cleaning the code after I finish everything.
+## Part 2
+Part 2 should have been simple (just another tweak to the rules), but I misunderstood the rules for this version initially and had to run it on the sample input to figure out my mistake.
