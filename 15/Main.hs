0f63bc5cd59828f69d3b19bc17fe08fac91afef5
S. John Trombley <s.john.trombley@gmail.com>
2020-12-16 13:55:40 -0700

    Day 15 functional but part 2 is slow. Added README.

--- 15/Main.hs
+++ 15/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Data.List (foldl')
+import Data.Map.Strict (member, (!), empty, insert)
+
+part1 l = print.head $ iterate (\m->(nextNum m):m) l!!(2020-length l)
+
+nextNum (n:ns) = if elem n ns then length (takeWhile (n/=) ns) + 1 else 0
+
+part2 l = print.fst $ foldl' nextNum2 (last l, buildMap l) [length l..29999999]
+
+buildMap l = foldl' (flip (uncurry insert)) empty $ zip (init l) [1..length l-1]
+-- \m (n, c)->insert n c m
+-- flip (uncurry insert)
+
+nextNum2 (last, prevs) count
+    = let prevs' = insert last count prevs
+      in if member last prevs then (count-prevs!last, prevs') else (0, prevs')
+
+main = do putStr "Part 1:\n\t"
+          part1 $ reverse [2, 0, 6, 12, 1, 3]
+          putStr "Part 2:\n\t"
+          part2 [2, 0, 6, 12, 1, 3]
